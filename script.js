3684bcbef1b7e437b69d8649266f31659f6e81bf
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-06-18 11:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let container = document.getElementById("line");
+// let container = document.getElementById("line");
 
-let innerContent = `<div></div>`;
+// let innerContent = `<div></div>`;
 
-container.innerHTML = innerContent;
+// container.innerHTML = innerContent;
